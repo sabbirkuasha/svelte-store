c4c0091c1841d71ec4a5a420dea42673679b2360
sabbirz <sabbir.kuasha@gmail.com>
2022-07-15 14:30:37 +0600

    Initial file structure
    initial file structure for learning svelte store

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script>
-
+  import Cart from "./Cart/Cart.svelte";
+  import Products from "./Products/Products.svelte";
 </script>
 
 <main>
-  <h1 class="text-3xl">this is basic</h1>
-  <button class="btn btn-primary">The Button</button>
+  <Cart/>
+  <Products/>
 </main>
 
 <style>
